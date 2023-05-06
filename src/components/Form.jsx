7b207868073539ac4a568648b7792ed66515ebaf
JaredMZ <jaredcv04@gmail.com>
2023-05-05 21:49:26 -0600

    random url function

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,20 @@
 import "../css/Form.css";
+import memesData from "../data/memesData";
 
 export default function Form() {
+  function getMemeImage() {
+    const memes = memesData.data.memes;
+    const randomIndex = Math.floor(Math.random() * memes.length);
+    const imgUrl = memes[randomIndex].url;
+
+    console.log(imgUrl);
+  }
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    getMemeImage();
+  }
+
   return (
     <form className="create--form" action="">
       <div className="create--subcontainer">
@@ -8,7 +22,9 @@ export default function Form() {
         <input className="create--form__input" type="text" />
       </div>
       <div className="create--subcontainer">
-        <button className="create--form__button">Get a new image</button>
+        <button className="create--form__button" onClick={handleClick}>
+          Get a new image
+        </button>
       </div>
     </form>
   );
