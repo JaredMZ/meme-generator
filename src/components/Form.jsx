7b255e94c4e277a6e9f45c081075348b1aa0eaac
JaredMZ <jaredcv04@gmail.com>
2023-05-06 22:51:56 -0600

    add change img function with usestate

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,31 +1,37 @@
+import { useState } from "react";
 import "../css/Form.css";
 import memesData from "../data/memesData";
 
 export default function Form() {
+  const [memeImage, setMemeImage] = useState("");
+
   function getMemeImage() {
     const memes = memesData.data.memes;
     const randomIndex = Math.floor(Math.random() * memes.length);
     const imgUrl = memes[randomIndex].url;
 
-    console.log(imgUrl);
+    setMemeImage(imgUrl);
   }
 
   const handleClick = (event) => {
     event.preventDefault();
     getMemeImage();
-  }
+  };
 
   return (
-    <form className="create--form" action="">
-      <div className="create--subcontainer">
-        <input className="create--form__input" type="text" />
-        <input className="create--form__input" type="text" />
-      </div>
-      <div className="create--subcontainer">
-        <button className="create--form__button" onClick={handleClick}>
-          Get a new image
-        </button>
-      </div>
-    </form>
+    <div>
+      <form className="create--form" action="">
+        <div className="create--subcontainer">
+          <input className="create--form__input" type="text" />
+          <input className="create--form__input" type="text" />
+        </div>
+        <div className="create--subcontainer">
+          <button className="create--form__button" onClick={handleClick}>
+            Get a new image
+          </button>
+        </div>
+      </form>
+      <img src={memeImage} alt="" />
+    </div>
   );
 }
